cd5b6bdaa436c615513ac2510da1b9358cd91df5
Massimo Biagioli <massimo.biagioli@ideato.info>
2022-03-21 14:28:40 +0100

    change index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express()
 
 app.get('/', (req, res) => {
-    return res.json({ message: 'Hello CI v1.6' });
+    return res.json({ message: 'Hello CI v1.7' });
 });
 
 app.listen(4000, () => console.log("Server started on port 4000"));
